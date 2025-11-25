a7864afb57d390e91b1159666943d1ae1da25d92
Honeycat <andrew.tsakas@gmail.com>
2025-11-25 21:22:20 +1000

    Update transmutation.c

--- boards/shields/nice_view_gem/assets/animations/transmutation.c
+++ boards/shields/nice_view_gem/assets/animations/transmutation.c
@@ -1,3 +1,10 @@
+#include <lvgl.h>
+
+#ifndef LV_ATTRIBUTE_MEM_ALIGN
+#define LV_ATTRIBUTE_MEM_ALIGN
+#endif
+
+
 
 #ifndef LV_ATTRIBUTE_IMG_TRANSMUTATION_01
 #define LV_ATTRIBUTE_IMG_TRANSMUTATION_01
